ee28c74b0b21fcaaabb5fe46ed5e68b407f3182e
Anna Kholod <uashop89@gmail.com>
2019-10-20 00:19:49 +0300

    Updates

---
+++ precache-manifest.3e79448fab373e6b0f332149308830f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ad87a8cb981c2c12ec7f10b5bfe021",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "81a117e4abe5426dd65a",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.8a636476.chunk.css"
+  },
+  {
+    "revision": "78f88e27bb0e8f36256f",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.d568e52e.chunk.js"
+  },
+  {
+    "revision": "81a117e4abe5426dd65a",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.239f2fa9.chunk.js"
+  },
+  {
+    "revision": "911313b77b6cc8e3fc26",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime-main.8874a67c.js"
+  }
+]);
